1cdbb0c407ca0666366ede47f74be67ece0018a0
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-08-24 20:41:32 -0400

    created new repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
+import React, { useState } from 'react';
+import ContactList from 'components/ContactList';
+import Filter from 'components/Filter';
+import ContactForm from './ContactForm';
+import { Container } from './App.styled';
+import storage from '../storage';
+import { useEffect } from 'react';
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const storedContacts = storage.load('contacts');
+
+    if (storedContacts && Array.isArray(storedContacts)) {
+      setContacts(storedContacts);
+    }
+  }, [setContacts]);
+
+  const handleNewContact = newContact => {
+    const updatedContacts = [...contacts, newContact];
+    setContacts(updatedContacts);
+    storage.save('contacts', updatedContacts);
+  };
+
+  const handleFilter = event => {
+    const filterValue = event.target.value;
+    setFilter(filterValue);
+  };
+
+  const handleDelete = id => {
+    console.log({ id });
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(updatedContacts);
+    storage.save('contacts', updatedContacts);
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact?.name?.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm handleNewContact={handleNewContact} contacts={contacts} />
+      <h2>Contacts</h2>
+
+      <Filter onChange={handleFilter} value={filter} />
+      <ContactList contacts={filteredContacts} handleDelete={handleDelete} />
+    </Container>
   );
 };
+
+export default App;
---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+// export const LiList = styled.li`
+//   display: flex;
+//   margin-bottom: 12px;
+//   align-items: center;
+// `;
+
+// export const SpanList = styled.span`
+//   margin-left: 5px;
+// `;
+
+// export const Button = styled.button`
+//   cursor: pointer;
+//   padding: 2px 6px;
+//   background-color: white;
+//   transition-duration: 0.4s;
+//   border-radius: 5px;
+//   border: 1px solid;
+//   margin-left: 13px;
+//   &:hover {
+//     background-color: rgb(43, 159, 243);
+//   }
+// `;
